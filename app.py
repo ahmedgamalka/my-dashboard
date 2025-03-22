1cb7233cc9324ac70e445aa16496611296537388
Ahmed G. <68081558+ahmedgamalka@users.noreply.github.com>
2025-03-22 06:47:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -325,6 +325,10 @@ def main():
             unsafe_allow_html=True
         )
 
+        if st.sidebar.button("🚪 Logout"):
+        st.session_state.pop("username", None)
+        st.experimental_rerun()
+
         if page == "Risk Management":
             risk_management_page(journal_file)
         elif page == "Add Trade":
